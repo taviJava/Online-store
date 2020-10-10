1067be3a26089a439f2c5da28ae842e8148e1fdd
Lavinia12 <gabrielalaviniay@gmail.com>
2020-10-10 11:14:30 +0300

    CategoryModel

---
+++ demo/src/main/java/com/sda/demo/model/CategoryModel.java
@@ -0,0 +1,43 @@
+package com.sda.demo.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CategoryModel {
+    private Long id;
+    private String name;
+    private CategoryModel parent;
+    private List<CategoryModel> subcategories =  new ArrayList<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public CategoryModel getParent() {
+        return parent;
+    }
+
+    public void setParent(CategoryModel parent) {
+        this.parent = parent;
+    }
+
+    public List<CategoryModel> getSubcategories() {
+        return subcategories;
+    }
+
+    public void setSubcategories(List<CategoryModel> subcategories) {
+        this.subcategories = subcategories;
+    }
+}
