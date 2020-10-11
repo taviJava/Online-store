c7bad16a6abc46adb2249a12c57cc6d228916aad
DESKTOP-L9IV3IL\Popo <popovici.bogdan1234@yahoo.com>
2020-10-11 10:33:28 +0300

    userdto

--- demo/src/main/java/com/sda/demo/dto/UserDto.java
+++ demo/src/main/java/com/sda/demo/dto/UserDto.java
@@ -1,4 +1,73 @@
 package com.sda.demo.dto;
 
+import com.sda.demo.persitance.model.AdressModel;
+import com.sda.demo.persitance.model.RoleModel;
+
+import java.util.List;
+
 public class UserDto {
+
+    private long id;
+    private String password;
+    private String newPassword;
+    private String email;
+    private String url;
+    private AdressModel adress;
+    private List<RoleDto> roleList;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getNewPassword() {
+        return newPassword;
+    }
+
+    public void setNewPassword(String newPassword) {
+        this.newPassword = newPassword;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public AdressModel getAdress() {
+        return adress;
+    }
+
+    public void setAdress(AdressModel adress) {
+        this.adress = adress;
+    }
+
+    public List<RoleDto> getRoleList() {
+        return roleList;
+    }
+
+    public void setRoleList(List<RoleDto> roleList) {
+        this.roleList = roleList;
+    }
 }
