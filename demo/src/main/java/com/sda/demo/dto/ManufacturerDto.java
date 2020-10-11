5239c8697ab9a880805336c20986449823076164
Lavinia12 <gabrielalaviniay@gmail.com>
2020-10-11 10:18:44 +0300

    no message

--- demo/src/main/java/com/sda/demo/dto/ManufacturerDto.java
+++ demo/src/main/java/com/sda/demo/dto/ManufacturerDto.java
@@ -1,4 +1,31 @@
 package com.sda.demo.dto;
 
 public class ManufacturerDto {
+    private long id;
+    private String name;
+    private ProductDto productDto;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ProductDto getProductDto() {
+        return productDto;
+    }
+
+    public void setProductDto(ProductDto productDto) {
+        this.productDto = productDto;
+    }
 }
