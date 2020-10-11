b89b635df0897fa749dee7319f177a115b415c4c
taviJava <tavi.zorila@gmail.com>
2020-10-11 12:38:32 +0300

    category service

--- demo/src/main/java/com/sda/demo/service/CategoryService.java
+++ demo/src/main/java/com/sda/demo/service/CategoryService.java
@@ -22,11 +22,62 @@ public class CategoryService {
              CategoryDto categoryDto = new CategoryDto();
              categoryDto.setId(categoryModel.getId());
              categoryDto.setName(categoryModel.getName());
-
+             List<CategoryModel> subCategories = categoryModel.getSubcategories();
+             List<CategoryDto> categoryDtos = new ArrayList<>();
+             for (CategoryModel subCategory: subCategories){
+                 CategoryDto subCategoryDto = new CategoryDto();
+                 subCategoryDto.setId(subCategory.getId());
+                 subCategoryDto.setName(subCategory.getName());
+                 categoriesDto.add(subCategoryDto);
+             }
+             categoryDto.setSubcategories(categoryDtos);
              categoriesDto.add(categoryDto);
          }
          return categoriesDto;
     }
 
+    private void add(CategoryDto categoryDto){
+        CategoryModel categoryModel = new CategoryModel();
+        categoryModel.setName(categoryDto.getName());
+        List<CategoryModel> subCategories = new ArrayList<>();
+        for (CategoryDto subCategory: categoryDto.getSubcategories()){
+            CategoryModel subCategoryModel = new CategoryModel();
+            subCategoryModel.setId(subCategory.getId());
+            subCategoryModel.setName(subCategory.getName());
+            subCategories.add(subCategoryModel);
+        }
+        categoryModel.setSubcategories(subCategories);
+    }
+ public CategoryDto getCategory(long id){
+CategoryModel categoryModel = categoryRepository.findById(id).orElse(null);
+CategoryDto categoryDto = new CategoryDto();
+categoryDto.setId(categoryModel.getId());
+categoryDto.setName(categoryModel.getName());
+         List<CategoryModel> subCategories = categoryModel.getSubcategories();
+         List<CategoryDto> subCategoryDtos = new ArrayList<>();
+         for (CategoryModel subCategory: subCategories){
+             CategoryDto subCategoryDto = new CategoryDto();
+             subCategoryDto.setId(subCategory.getId());
+             subCategoryDto.setName(subCategory.getName());
+             subCategoryDtos.add(subCategoryDto);
+         }
+         categoryDto.setSubcategories(subCategoryDtos);
+         return categoryDto;
+ }
+public void delete(long id){
+      categoryRepository.deleteById(id);
+}
 
+public void update(CategoryDto categoryDto){
+        CategoryModel categoryModel = categoryRepository.findById(categoryDto.getId()).orElse(null);
+    categoryModel.setName(categoryDto.getName());
+    List<CategoryModel> subCategories = new ArrayList<>();
+    for (CategoryDto subCategory: categoryDto.getSubcategories()){
+        CategoryModel subCategoryModel = new CategoryModel();
+        subCategoryModel.setId(subCategory.getId());
+        subCategoryModel.setName(subCategory.getName());
+        subCategories.add(subCategoryModel);
+    }
+    categoryModel.setSubcategories(subCategories);
+}
 }
