805a133653e03c1dcd805b298ea4a8474fddfeba
AdrianPatruSDA <adrianpatru1986@gmail.com>
2020-10-11 10:41:26 +0300

    am pus orderdto

--- demo/src/main/java/com/sda/demo/dto/OrderDto.java
+++ demo/src/main/java/com/sda/demo/dto/OrderDto.java
@@ -1,4 +1,54 @@
 package com.sda.demo.dto;
 
+import java.sql.Date;
 public class OrderDto {
+    private long id;
+    private String username;
+    private double totalCost;
+    private String deliveryAddress;
+    private String userAddress;
+    private Date orderDate;
+    private String status;
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public double getTotalCost() {
+        return totalCost;
+    }
+    public void setTotalCost(double totalCost) {
+        this.totalCost = totalCost;
+    }
+    public String getDeliveryAddress() {
+        return deliveryAddress;
+    }
+    public void setDeliveryAddress(String deliveryAddress) {
+        this.deliveryAddress = deliveryAddress;
+    }
+    public String getUserAddress() {
+        return userAddress;
+    }
+    public void setUserAddress(String userAddress) {
+        this.userAddress = userAddress;
+    }
+    public Date getOrderDate() {
+        return orderDate;
+    }
+    public void setOrderDate(Date orderDate) {
+        this.orderDate = orderDate;
+    }
+    public String getStatus() {
+        return status;
+    }
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
