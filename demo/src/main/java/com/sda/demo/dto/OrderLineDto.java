9ccc7c355d01855951bd3ef9a9364221bf033697
taviJava <tavi.zorila@gmail.com>
2020-10-11 10:26:18 +0300

    SI ORDERLINE

--- demo/src/main/java/com/sda/demo/dto/OrderLineDto.java
+++ demo/src/main/java/com/sda/demo/dto/OrderLineDto.java
@@ -1,4 +1,42 @@
 package com.sda.demo.dto;
 
+
+
 public class OrderLineDto {
+    private long id;
+    private ProductDto product;
+    private int productsQuantity;
+    private double productPrice;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public ProductDto getProduct() {
+        return product;
+    }
+
+    public void setProduct(ProductDto product) {
+        this.product = product;
+    }
+
+    public int getProductsQuantity() {
+        return productsQuantity;
+    }
+
+    public void setProductsQuantity(int productsQuantity) {
+        this.productsQuantity = productsQuantity;
+    }
+
+    public double getProductPrice() {
+        return productPrice;
+    }
+
+    public void setProductPrice(double productPrice) {
+        this.productPrice = productPrice;
+    }
 }
