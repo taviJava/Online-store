21c340a77eac84f62955599bb9abf39619f42e2c
taviJava <tavi.zorila@gmail.com>
2020-10-12 12:37:17 +0300

    no message

---
+++ demo/src/main/java/com/sda/demo/controller/PrivilegeController.java
@@ -0,0 +1,40 @@
+package com.sda.demo.controller;
+
+import com.sda.demo.dto.AddressDto;
+import com.sda.demo.dto.PrivilegeDto;
+import com.sda.demo.service.PrivilegeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin
+public class PrivilegeController {
+    @Autowired
+    private PrivilegeService privilegeService;
+
+    @PostMapping("/privilege")
+    public void add(@RequestBody PrivilegeDto privilegeDto) {
+        privilegeService.add(privilegeDto);
+    }
+    @DeleteMapping("/privilege/{id}")
+    public void delete(@PathVariable(name = "id") Long id) {
+        privilegeService.delete(id);
+    }
+    @GetMapping("/privilege")
+    public List<PrivilegeDto> getAll() {
+        return privilegeService.getAll();
+    }
+
+    @GetMapping("/privilege/{id}")
+    public PrivilegeDto get(@PathVariable(name = "id") Long id) {
+        return privilegeService.getPrivilege(id);
+    }
+
+    @PutMapping("/privilege")
+    public void update(@RequestBody PrivilegeDto privilegeDto) {
+        privilegeService.update(privilegeDto);
+
+    }
+}
