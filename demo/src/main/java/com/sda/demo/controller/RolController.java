90fff6a96736894d4c02e852d9a82e73ea7b007d
Lavinia12 <gabrielalaviniay@gmail.com>
2020-10-12 14:05:05 +0300

    no message

---
+++ demo/src/main/java/com/sda/demo/controller/RolController.java
@@ -0,0 +1,41 @@
+package com.sda.demo.controller;
+
+import com.sda.demo.dto.RoleDto;
+import com.sda.demo.service.RoleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin
+public class RolController {
+    @Autowired
+    private RoleService roleService;
+
+    @PostMapping("/role")
+    public void add(@RequestBody RoleDto roleDto) {
+        roleService.add(roleDto);
+    }
+
+    @DeleteMapping("/role/{id}")
+    public void delete(@PathVariable(name = "id") Long id) {
+        roleService.delete(id);
+    }
+
+    @GetMapping("/role")
+    public List<RoleDto> getRoles() {
+        return roleService.getAll();
+    }
+
+    @GetMapping("/role/{id}")
+    public RoleDto getRole(@PathVariable(name = "id") Long id) {
+        return roleService.getRole(id);
+    }
+
+    @PutMapping("/role")
+    public void update(@RequestBody RoleDto roleDto) {
+        roleService.update(roleDto);
+
+    }
+}
