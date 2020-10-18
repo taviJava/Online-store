fa1e5c9d2c7a9d5b202859a40db5b000585d6ce1
taviJava <tavi.zorila@gmail.com>
2020-10-18 12:22:28 +0300

    -

---
+++ fonlinestore/src/app/users/service/auth-guard2.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import {CanActivate, Router} from '@angular/router';
+import {User} from '../model/user';
+import {BehaviorSubject} from 'rxjs';
+import {HttpClient} from '@angular/common/http';
+import {UserService} from './user.service';
+import {AuthenticationService} from './authentication.service';
+import {Role} from '../../security/model/role';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard2Service implements CanActivate {
+  TOKEN_SESSION_ATTRIBUTE_NAME = 'authenticatedUserToken';
+  USER_DATA_SESSION_ATTRIBUTE_NAME = 'authenticatedUserData';
+  public password: string;
+  public user: User = new User();
+  roles: Role[] = [];
+  public isLoggedIn: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+  constructor(public auth: AuthenticationService, public router: Router, public userService: UserService) {
+    this.user = new User();
+    this.roles = [];
+  }
+  canActivate(): boolean {
+    const privS = 'Full Control';
+   // const user = JSON.parse(sessionStorage.getItem(this.USER_DATA_SESSION_ATTRIBUTE_NAME));
+    if (!this.auth.isUserLoggedIn()) {
+      this.router.navigate(['login']);
+      return false;
+    }else if (this.auth.isUserLoggedIn() && this.auth.userPrivilegeAdm() ) {
+    return true;
+    }
+    return false;
+  }
+}
